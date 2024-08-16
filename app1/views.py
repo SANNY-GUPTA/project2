9c329a613d8b6e98f022b5f27b5d18b088b51510
sanny gupta <sannygupta79@gmail.com>
2024-08-16 23:01:25 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def sanny(request):
+    return render(request,'sanny.html')
+
+
+def Ram(request):
+    return render(request,'Ram.html')
+
+def good(request):
+    return render(request,'good.html')
+
+def ok(request):
+    return render(request,'ok.html')
+# Create your views here.
